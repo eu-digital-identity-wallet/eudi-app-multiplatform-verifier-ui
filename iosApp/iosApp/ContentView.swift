723c7b65040ba2002344f9116321e2b7318a6f78
Stylianos Tzouvaras <stylianos.tzouvaras@netcompany.com>
2025-05-23 19:07:20 +0300

    style: Code Style Updates
    This commit updates the code style and formatting in the following files:
    - `iosApp/Configuration/Config.xcconfig`: Updates `TEAM_ID`, `PRODUCT_BUNDLE_IDENTIFIER`, and `MARKETING_VERSION`.
    - `iosApp/iosApp/ContentView.swift`: Minor formatting changes.
    - `iosApp/iosApp/iOSApp.swift`: Minor formatting changes.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,19 +3,16 @@ import SwiftUI
 import VerifierApp
 
 struct ComposeView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
-    }
-
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+  func makeUIViewController(context: Context) -> UIViewController {
+    MainViewControllerKt.MainViewController()
+  }
+  
+  func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
 struct ContentView: View {
-    var body: some View {
-        ComposeView()
-                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
-    }
+  var body: some View {
+    ComposeView()
+      .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
+  }
 }
-
-
-
