841cde9dbc7fa54e39da8a2bd1e0a658851f11bc
Giannis Stamatopoulos <giannis.stamatopoulos@netcompany.com>
2025-07-18 16:44:28 +0300

    Feat: Add camera and photo library usage descriptions for iOS
    This commit adds the `NSCameraUsageDescription` and `NSPhotoLibraryUsageDescription` keys to the `Info.plist` file for the iOS app. These descriptions are required by iOS to inform the user why the app needs access to the camera and photo library respectively.
    
    Additionally, the `VerifierApp.swift` file was modified to ignore the safe area at the bottom edge for the `ContentView`.

--- iosVerifierApp/iosVerifierApp/Sources/Presentation/ContentView.swift
+++ iosVerifierApp/iosVerifierApp/Sources/Presentation/ContentView.swift
@@ -3,15 +3,15 @@ import SwiftUI
 import VerifierApp
 
 struct ComposeView: UIViewControllerRepresentable {
-  func makeUIViewController(context: Context) -> UIViewController {
-    ContainerViewControllerKt.ContainerViewController()
-  }
-  func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    func makeUIViewController(context: Context) -> UIViewController {
+        ContainerViewControllerKt.ContainerViewController()
+    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
 struct ContentView: View {
-  var body: some View {
-    ComposeView()
-      .ignoresSafeArea(.keyboard)
-  }
+    var body: some View {
+        ComposeView()
+            .ignoresSafeArea(.keyboard)
+    }
 }
